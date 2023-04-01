7740dd17a6ca3d2154c4c02d2df520a33cc86465
choumar2001 <65606795+choumar2001@users.noreply.github.com>
2023-04-01 21:40:04 +0200

    added feature to capture objects in the camera frame

--- main.py
+++ main.py
@@ -10,13 +10,23 @@ time.sleep(1)
 first_frame = None
 status_list = []
 
+# used for the number of images captured
+count = 1
+
 while True:
+    # resets the status of the object on every iteration
     status = 0
 
     # starts taking the video and checks if it is taking the video
     check, frame = video.read()
 
-    # makes the frame to a gray color and then
+    # stores images captured from the frame
+    cv.imwrite(f"images/{count}.png", frame)
+
+    # updates the number of images taken
+    count += 1
+
+    # makes the frame to a gray color and then makes it blurry to be able to blur out the background
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     gray_frame_gau = cv.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -27,9 +37,11 @@ while True:
     # used to compare the old frame with the new frames
     delta_frame = cv.absdiff(first_frame, gray_frame_gau)
 
-
+    # focuses the white color on the object and blacks out the background
     thresh_frame = cv.threshold(delta_frame, 65, 255, cv.THRESH_BINARY)[1]
-    dil_frame = cv.dilate(thresh_frame, None, iterations=3)
+
+    # makes sure that the object only is white
+    dil_frame = cv.dilate(thresh_frame, None, iterations=2)
     cv.imshow("My video", dil_frame)
 
     contours, check = cv.findContours(dil_frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
@@ -43,17 +55,22 @@ while True:
         if rect.any():
             status = 1
 
+    # stores the status of the object in a list to check if it is still in frame
     status_list.append(status)
     status_list = status_list[-2:]
 
+    # checks if an object is still in frame then sends an email if not
     if status_list[0] == 1 and status_list[1] == 0:
         send_email()
     print(status_list)
 
+    # presents the colored normal frame
     cv.imshow("video", frame)
     key = cv.waitKey(1)
 
+    # quits the program when q is pressed
     if key == ord("q"):
         break
 
+# deletes the video capture
 video.release()
